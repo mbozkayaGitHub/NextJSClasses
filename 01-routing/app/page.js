85341923625cf90e63e07782306507bfba9f86b1
mbozkayaGitHub <mbozkaya27@gmail.com>
2024-03-28 13:22:41 +0300

    update

--- 01-routing/app/page.js
+++ 01-routing/app/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div>
+      <div> Hello World</div>
+      <Link href="/about"> About Page </Link>
+    </div>
+  );
+};
 
-export default page
+export default page;
